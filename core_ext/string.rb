53fcc61699916b9c7aea98f20b0dd57cd5d271ce
Albert Llop <mrsimo@gmail.com>
2010-06-13 16:46:23 +0200

    Parsing size-like strings into floats in bytes

--- core_ext/string.rb
+++ core_ext/string.rb
@@ -4,4 +4,18 @@ class String
   def to_time
     Chronic.parse(self)
   end
+  
+  def to_size
+    amount,unit = self.split(" ")
+    case unit.downcase
+    when "bytes","b","by"
+      amount.to_f
+    when "mb","megabytes","mbs","m","mi","mo"
+      amount.to_f * 1024
+    when "gb","gigabytes","gbs","g","gi"
+      amount.to_f * 1024 * 1024
+    else
+      self
+    end
+  end
 end
